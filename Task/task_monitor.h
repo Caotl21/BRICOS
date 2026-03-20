060f5b147c098d9effb2f082a1ca2b09281a6231
Caotl21 <caotailai_21@163.com>
2026-03-21 00:51:01 +0800

    [feat]:增加CPU使用率监控任务

---
+++ Task/task_monitor.h
@@ -0,0 +1,16 @@
+#ifndef __TASK_MONITOR_H
+#define __TASK_MONITOR_H
+
+#include <stdint.h>
+#include "FreeRTOS.h"
+#include "task.h"
+
+#define MONITOR_TASK_PRIO      1
+#define MONITOR_STK_SIZE       256
+#define MONITOR_PERIOD_MS      1000
+
+extern TaskHandle_t Monitor_Task_Handler;
+
+void Task_Monitor_Init(void);
+
+#endif /* __TASK_MONITOR_H */
